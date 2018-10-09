74884aa9a1b9a52f85fe787ca8e5f535d0381e17
William <william@fontrip.com>
2018-10-09 17:05:44 +0800

    修改 AddHeaders 中介層

--- app/Http/Middleware/AddHeaders.php
+++ app/Http/Middleware/AddHeaders.php
@@ -9,7 +9,9 @@ class AddHeaders
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', env('ACCESS_CONTROL_ALLOW_ORIGIN', '*'));
+        $response
+            ->header('Access-Control-Allow-Origin', env('ACCESS_CONTROL_ALLOW_ORIGIN', '*'))
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
         return $response;
     }
